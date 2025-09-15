a519536ff827ca6d902b6f3185817e470e42f6d8
sentryc0201 <yc020102@gmail.com>
2025-09-15 12:49:38 +0900

    [FIX] HIGH -> LOW

--- 05_example_2/05_example_2.ino
+++ 05_example_2/05_example_2.ino
@@ -2,22 +2,22 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW); 
+  digitalWrite(PIN_LED, HIGH); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PIN_LED, HIGH);
+  digitalWrite(PIN_LED, LOW);
   delay(1000);
 
   for (int i = 0; i < 5; i++){
-    digitalWrite(PIN_LED, LOW);
-    delay(100);
     digitalWrite(PIN_LED, HIGH);
+    delay(100);
+    digitalWrite(PIN_LED, LOW);
     delay(100);
   }
 
-  digitalWrite(PIN_LED, LOW);
+  digitalWrite(PIN_LED, HIGH);
 
   while(1){}
 }
