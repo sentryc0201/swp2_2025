0a19cb78b04693ec6cc4eaedfd671f70fa75cb39
sentryc0201 <yc020102@gmail.com>
2025-09-15 12:23:23 +0900

    Update 05_example_2

---
+++ 05_example_2/05_example_2.ino
@@ -0,0 +1,26 @@
+# define PIN_LED 13
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW); 
+  Serial.begin(115200); // Initialize serial port
+  Serial.println("start");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++){
+    digitalWrite(PIN_LED, LOW);
+    delay(100);
+    digitalWrite(PIN_LED, HIGH);
+    delay(100);
+    Serial.println(i);  
+  }
+
+  digitalWrite(PIN_LED, LOW);
+
+  while(1){}
+}
